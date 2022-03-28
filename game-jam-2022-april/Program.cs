870db83d713a2c545eb833d2404ee922ba20d122
Jan Schwarz <jan.schwarz@2mt.info>
2022-03-28 13:11:11 +0200

    add project template

---
+++ game-jam-2022-april/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace game_jam_2022_april
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
